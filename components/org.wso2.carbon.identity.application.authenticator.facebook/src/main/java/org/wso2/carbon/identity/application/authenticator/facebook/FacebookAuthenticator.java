2897ad4b18e723031c060a2061f8cd34b5e9e87a
kasungayan <kbandara149@gmail.com>
2016-08-15 23:45:28 +0530

    Fixing IDENTITY-4769

--- components/org.wso2.carbon.identity.application.authenticator.facebook/src/main/java/org/wso2/carbon/identity/application/authenticator/facebook/FacebookAuthenticator.java
+++ components/org.wso2.carbon.identity.application.authenticator.facebook/src/main/java/org/wso2/carbon/identity/application/authenticator/facebook/FacebookAuthenticator.java
@@ -158,7 +158,7 @@ public class FacebookAuthenticator extends AbstractApplicationAuthenticator impl
                 scope = FacebookAuthenticatorConstants.EMAIL;
             }
 
-            String callbackUrl = IdentityUtil.getServerURL(FrameworkConstants.COMMONAUTH, true, true);
+            String callbackUrl = authenticatorProperties.get(FacebookAuthenticatorConstants.FB_CALLBACK_URL);
 
             String state = context.getContextIdentifier() + "," + FacebookAuthenticatorConstants.FACEBOOK_LOGIN_TYPE;
 
@@ -198,7 +198,7 @@ public class FacebookAuthenticator extends AbstractApplicationAuthenticator impl
             String tokenEndPoint = getTokenEndpoint();
             String fbAuthUserInfoUrl = getUserInfoEndpoint();
 
-            String callbackUrl = IdentityUtil.getServerURL(FrameworkConstants.COMMONAUTH, true, true);
+            String callbackUrl = authenticatorProperties.get(FacebookAuthenticatorConstants.FB_CALLBACK_URL);
 
             String code = getAuthorizationCode(request);
             String token = getToken(tokenEndPoint, clientId, clientSecret, callbackUrl, code);
